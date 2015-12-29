d1ddf608b9478d51bbec3fda19923496792a9516
marius92mc <marius_92m@yahoo.com>
2015-12-29 17:36:06 +0200

    Add feature to print time of disturbances, default is 1000, or specified as argument

--- real_time_audio.py
+++ real_time_audio.py
@@ -1,3 +1,4 @@
+import time
 import ui_plot
 import sys
 import numpy
@@ -5,7 +6,7 @@ from PyQt4 import QtCore, QtGui
 import PyQt4.Qwt5 as Qwt
 from recorder import *
 
-
+gBigValue = 1000
 
 def plotSomething():
     if SR.newAudio==False: 
@@ -14,8 +15,18 @@ def plotSomething():
     c.setData(xs,ys)
     uiplot.qwtPlot.replot()
     SR.newAudio=False
+    if len(ys) > 0 and ys[0] > gBigValue: 
+        print time.strftime("%I:%M:%S %p  %B %d")
 
 if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        if isinstance(int(sys.argv[1]), int):
+            gBigValue = int(sys.argv[1])
+        else:
+            print "Not an integer value...\nTerminated."
+            sys.exit()
+    print "Disturbances greater than ", gBigValue, "at time..."
+    
     app = QtGui.QApplication(sys.argv)
     
     win_plot = ui_plot.QtGui.QMainWindow()
